19a4b11fe3fbcadda3151c818979be637fbbd34e
Amero <vian8416@163.com>
2021-08-05 10:15:39 +0800

    Complete Alter

--- Double_Link_list/Double_Link_list/Double_Link_list.cpp
+++ Double_Link_list/Double_Link_list/Double_Link_list.cpp
@@ -32,6 +32,7 @@ DoubleLinkList<DataType>::DoubleLinkList()
 	first->next = NULL;
 	first->head = NULL;
 }
+
 template<class DataType>
 DoubleLinkList<DataType>::DoubleLinkList(int n, DataType a[])
 {
@@ -54,6 +55,20 @@ DoubleLinkList<DataType>::DoubleLinkList(int n, DataType a[])
 		r->next = p;
 		r = p;
 	}
+}
+
+template<class DataType>
+DoubleLinkList<DataType>::~DoubleLinkList()
+{
+	DoubleNode<DataType> *p = first;
+	while (p != NULL)
+	{
+		DoubleNode<DataType> *q = new DoubleNode<DataType>;
+		q = p;
+		p = p->next;
+		delete q;
+	}
+
 }
 
 template<class DataType>
@@ -118,13 +133,59 @@ DataType DoubleLinkList<DataType>::GetElement(int i)
 template<class DataType>
 void DoubleLinkList<DataType>::InsertElement(int index, DataType x)
 {
+	DoubleNode<DataType> *p = first;
+	int j = 0;
+	if (index < 0 || index == 0)
+	{
+		throw "输入错误";
+	}
+	while (p != NULL && j < index-1)
+	{
+		p = p->next;
+		j++;
+	}
+	if (p == NULL)
+	{
+		throw "输入有误";
+	}
+	else
+	{
+		DoubleNode<DataType> *s = new DoubleNode<DataType>;
+		s->data = x;
+		s->next = p->next;
+		if (p->next != NULL)
+		{
+			p->next->head = s;
+		}
+		s->head = p;
+		p->next = s;
+	}
 
 }
 
 template<class DataType>
 DataType DoubleLinkList<DataType>::DeleteElement(int i)
 {
-
+	DoubleNode<DataType> *p = first;
+	int j = 0;
+	while (p != NULL && j < i - 1)
+	{
+		p = p->next;
+		j++;
+	}
+	if (p != NULL)
+	{
+		DoubleNode<DataType> *q = new DoubleNode<DataType>;
+		DataType x = p->next->data;
+		q = p->next;
+		p->next = q->next;
+		if (q->next != NULL)
+		{
+			q->next->head = p;
+		}
+		delete q;
+		return x;
+	}
 }
 
 template<class DataType>
@@ -139,25 +200,31 @@ void DoubleLinkList<DataType>::PrintList()
 	cout << endl;
 }
 
-template<class DataType>
-DoubleLinkList<DataType>::~DoubleLinkList()
-{
-}
-
 void show() 
 {
-	cout << "=====================" << endl;
+	cout << "===============================" << endl;
 }
+
 int main()
 {
-	int array[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int array[] = { 2,3,5,1,7,8,4,10,22,21 };
 	DoubleLinkList<int> TestLinkList = DoubleLinkList<int>(10,array);
 	TestLinkList.PrintList();
 	show();
+	cout << "Get Length:" << "  ";
 	cout << TestLinkList.GetLength() << endl;
 	show();
+	cout << "Get Element '1' Locate:" << "  ";
 	cout << TestLinkList.GetLocate(1) << endl;
 	show();
-	cout << TestLinkList.GetElement(18) << endl;
-
+	cout << "Get Locate '2' Element:" << "  ";
+	cout << TestLinkList.GetElement(2) << endl;
+	show();
+	cout << "Insert Element '32' to Locate '11':" << "  ";
+	TestLinkList.InsertElement(11, 31);
+	TestLinkList.PrintList();
+	show();
+	cout << "Delete Locate '11' Element:" << "  ";
+	TestLinkList.DeleteElement(11);
+	TestLinkList.PrintList();
 } 
